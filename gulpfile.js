663ff39b48976180481e47b4f826f3e8730cce8f
IshaDakota <adam@marchinglizard.net>
2017-05-22 20:26:57 -0500

    Changes for deployment

--- gulpfile.js
+++ gulpfile.js
@@ -64,15 +64,15 @@ gulp.task("js", function() {
 
 // Optimizes the images that exists
 gulp.task("images", function () {
-  return gulp.src("src/images/**")
-    .pipe($.changed("site/images"))
+  return gulp.src("src/assets/images/**")
+    .pipe($.changed("site/assets/images"))
     .pipe($.imagemin({
       // Lossless conversion to progressive JPGs
       progressive: true,
       // Interlace GIFs for progressive rendering
       interlaced: true
     }))
-    .pipe(gulp.dest("site/images"))
+    .pipe(gulp.dest("site/assets/images"))
     .pipe($.size({title: "images"}));
 });
 
